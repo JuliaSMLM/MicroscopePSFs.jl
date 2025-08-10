067c5870e0a64024ec300088e6208d5a313a7b0f
kiwibogo <kiwibogo@gmail.com>
2025-08-09 21:54:24 -0600

    add exfield_grad.jl for gradient calculations

--- dev/exfield_grad.jl
+++ dev/exfield_grad.jl
@@ -3,9 +3,7 @@ using MicroscopePSFs
 PSF=MicroscopePSFs
 using CairoMakie
 CM = CairoMakie
-using ImageView
-using GLMakie
-GM = GLMakie
+using Zygote
 
 na=1.35
 n=[1.406,1.52,1.33] # refractive indices (immersion , cover glass, sample medium)
@@ -160,16 +158,11 @@ CM.activate!()
 fig
 
 #simulate PSF
-#ringsz = [0.0, 0.51, 1.0]
-ringsz = [1]
-fθ = (x) -> phase_theta(x, na, n[1], ringsz) # 3D: [0.42, 0.61, 1.0]
-fϕ = (x) -> phase_phi(x)
 p=PSF.Focusfield(na,λ,n,pixelsize; ksize=128,excitationfield=[1.0,1*im],fθ=fθ,fϕ=fϕ,zstage=0.0) 
-sz=50 
-#roi=[(y,x,z) for y=1:sz, x=1:sz, z=0:-0.04:-1.5] 
+sz=31 
 roi=[(y,x,z) for y=1:sz, x=1:sz, z=0:0] 
 
-pos_emitter = (sz/2+1,sz/2+1,0.0)
+pos_emitter = (sz/2+0.5,sz/2+0.5,0.0)
 p.electricfield = 'x'
 imx=PSF.pdf(p,roi,pos_emitter)
 p.electricfield = 'y'
@@ -177,54 +170,101 @@ imy=PSF.pdf(p,roi,pos_emitter)
 p.electricfield = 'z'
 imz=PSF.pdf(p,roi,pos_emitter)
 imall = imx.+imy.+imz;
-cc = Int(pos_emitter[1])
 #look at psf in x and y polarization
-fig = Figure(size = (600, 400))
+fig = Figure(size = (600, 160))
 ax = CM.Axis(fig[1, 1],title="Ex psf",aspect=1)
-hm = CM.heatmap!(ax, imx[cc,:,:],colormap=:inferno)
-hidedecorations!(ax)
-ax = CM.Axis(fig[1, 2],title="Ey psf",aspect=1)
-hm = CM.heatmap!(ax, imy[cc,:,:],colormap=:inferno)
-hidedecorations!(ax)
-ax = CM.Axis(fig[1, 3],title="Ez psf",aspect=1)
-hm = CM.heatmap!(ax, imz[cc,:,:],colormap=:inferno)
-hidedecorations!(ax)
-ax = CM.Axis(fig[1, 4],title="total psf",aspect=1)
-hm = CM.heatmap!(ax, imall[cc,:,:],colormap=:inferno)
-hidedecorations!(ax)
-ax = CM.Axis(fig[2, 1],title="Ex psf",aspect=1)
 hm = CM.heatmap!(ax, imx[:,:,1],colormap=:inferno)
 hidedecorations!(ax)
-ax = CM.Axis(fig[2, 2],title="Ey psf",aspect=1)
+ax = CM.Axis(fig[1, 2],title="Ey psf",aspect=1)
 hm = CM.heatmap!(ax, imy[:,:,1],colormap=:inferno)
 hidedecorations!(ax)
-ax = CM.Axis(fig[2, 3],title="Ez psf",aspect=1)
+ax = CM.Axis(fig[1, 3],title="Ez psf",aspect=1)
 hm = CM.heatmap!(ax, imz[:,:,1],colormap=:inferno)
 hidedecorations!(ax)
-ax = CM.Axis(fig[2, 4],title="total psf",aspect=1)
+ax = CM.Axis(fig[1, 4],title="total psf",aspect=1)
 hm = CM.heatmap!(ax, imall[:,:,1],colormap=:inferno)
 hidedecorations!(ax)
 CM.activate!()
 display(fig)
 
-using HDF5
-
-datadir = raw"C:\Users\Sheng\Documents\git\git_embl\MINFLUX-analysis\minflux demo\simdata/"
-filename = "exfield_3d_642_2D_4.h5"
-fh = h5open(datadir*filename, "w")
-write(fh, "imx", imx)
-write(fh, "imy", imy)
-write(fh, "imz", imz)
-write(fh, "imall", imall)
-write(fh, "ringsz", ringsz)
-write(fh, "phasemask", p.phasemask)
-close(fh)
 
 
-datadir = raw"C:\Users\Sheng\Documents\git\git_embl\MINFLUX-analysis\minflux demo\simdata/"
-filename = "exfield_3d_642_2D_phasemask.h5"
-fh = h5open(datadir*filename, "w")
-write(fh, "phasemask", p.phasemask)
-close(fh)
+
+function model_exfield(θ, (x,y,z), p;bin=1.0)
+    cor = [(y,x) for y=-0.5+0.5/bin:1/bin:0.5-0.5/bin, x=-0.5+0.5/bin:1/bin:0.5-0.5/bin]
+
+    u0 = 0.0
+    for j in eachindex(cor)
+        Δx,Δy = cor[j]
+        p.electricfield = 'x'
+        imx = PSF.pdf(p,(x+Δx,y+Δy,z), (θ[1], θ[2], θ[3]))
+        p.electricfield = 'y'
+        imy = PSF.pdf(p,(x+Δx,y+Δy,z), (θ[1], θ[2], θ[3]))
+        p.electricfield = 'z'
+        imz = PSF.pdf(p,(x+Δx,y+Δy,z), (θ[1], θ[2], θ[3]))   
+        u0 += (imx+imy+imz)/bin
+    end
+    out = u0 
+    return out
+end
+
+
+
+
+θ = [pos_emitter...]
+psf = zeros(sz,sz)
+grad_x = zeros(sz,sz)
+grad_y = zeros(sz,sz)
+fimodel = (a, b) -> model_exfield(a, b, p)
+
+for j in eachindex(roi)
+    μ, grad = Zygote.forward_jacobian(θ -> fimodel(θ, roi[j]), θ)
+    grad_x[j] = -grad[2]
+    grad_y[j] = -grad[1]
+    psf[j] = μ
+end
+
+
+grad_x = reshape(grad_x, (sz, sz))
+grad_y = reshape(grad_y, (sz, sz))
+psf = reshape(psf, (sz, sz))
+
+
+fig = Figure(size = (600, 160))
+ax = CM.Axis(fig[1, 1],title="grad_x",aspect=1,yreversed=true)
+hm = CM.heatmap!(ax, grad_x',colormap=:inferno)
+hidedecorations!(ax)
+ax = CM.Axis(fig[1, 2],title="grad_y",aspect=1,yreversed=true)
+hm = CM.heatmap!(ax, grad_y',colormap=:inferno)
+hidedecorations!(ax)
+ax = CM.Axis(fig[1, 3],title="psf",aspect=1,yreversed=true)
+hm = CM.heatmap!(ax, psf',colormap=:inferno)
+hidedecorations!(ax)
+CM.activate!()
+display(fig)
+
+
+
+scanradius = 0.1 # micron
+Xscanpos = [0,-cos(pi/6),cos(pi/6),0].*scanradius
+Yscanpos = [1,-sin(pi/6),-sin(pi/6),0].*scanradius
+
+scanpos = [(Yscanpos[j]/pixelsize,Xscanpos[j]/pixelsize,0.0) for j in eachindex(Xscanpos)]
+
+pos_emitter = (0.0,0.0,0.0)
+Nscan = length(scanpos)
+θ = [pos_emitter...]
+psf = zeros(Nscan)
+grad_x = zeros(Nscan)
+grad_y = zeros(Nscan)
+fimodel = (a, b) -> model_exfield(a, b, p)
+
+
+for j in eachindex(scanpos)
+    μ, grad = Zygote.forward_jacobian(θ -> fimodel(θ, scanpos[j]), θ)
+    grad_x[j] = -grad[2]
+    grad_y[j] = -grad[1]
+    psf[j] = μ
+end
 
 
